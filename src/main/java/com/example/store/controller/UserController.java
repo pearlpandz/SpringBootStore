2ef3c46dfdceccb70ef7a44404f2d27f4d923872
pearlpandz <pearlpandzz@gmail.com>
2024-10-12 18:25:21 +0530

    CRUD Operation Completed

--- src/main/java/com/example/store/controller/UserController.java
+++ src/main/java/com/example/store/controller/UserController.java
@@ -3,10 +3,9 @@ package com.example.store.controller;
 import com.example.store.entity.User;
 import com.example.store.service.UserService;
 import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.http.HttpStatus;
 import org.springframework.http.ResponseEntity;
-import org.springframework.web.bind.annotation.GetMapping;
-import org.springframework.web.bind.annotation.RequestMapping;
-import org.springframework.web.bind.annotation.RestController;
+import org.springframework.web.bind.annotation.*;
 
 import java.util.List;
 
@@ -17,7 +16,31 @@ public class UserController {
     @Autowired
     private UserService userService;
 
-    @GetMapping("")
+    @PostMapping("")
+    public ResponseEntity<User> createUser(@RequestBody User user) throws Exception {
+        User userObj = null;
+        try {
+            userObj = userService.updateUser(user);
+        } catch (Exception e) {
+            throw new Exception(e.getMessage());
+        }
+        return new ResponseEntity<User> (userObj, HttpStatus.CREATED);
+    }
+
+    @PutMapping("/{id}")
+    public ResponseEntity<User> updateUser(@PathVariable("id") int userId, @RequestBody User user) throws Exception {
+        User userObj = user;
+        userObj.setUserId(userId);
+        User userRes = null;
+        try {
+            userRes = userService.updateUser(userObj);
+        } catch (Exception e) {
+            throw new Exception(e.getMessage());
+        }
+        return new ResponseEntity<User> (userRes, HttpStatus.CREATED);
+    }
+
+    @GetMapping("/list")
     public ResponseEntity<List<User>> getAllUsers() throws Exception {
         List<User> users = null;
         try {
@@ -27,5 +50,27 @@ public class UserController {
             throw new Exception(e.getMessage());
         }
         return ResponseEntity.ok(users);
+    }
+
+    @GetMapping("/{id}")
+    public ResponseEntity<User> updateUser(@PathVariable("id") int userId) throws Exception {
+        User user = null;
+        try {
+            user = userService.getUser(userId);
+        } catch (Exception e) {
+            throw new Exception(e.getMessage());
+        }
+        return new ResponseEntity<User> (user, HttpStatus.OK);
+    }
+
+    @DeleteMapping("/{id}")
+    public ResponseEntity<User> deleteUser(@PathVariable("id") int userId) throws Exception {
+        User user = null;
+        try {
+            user = userService.deleteUser(userId);
+        } catch (Exception e) {
+            throw new Exception(e.getMessage());
+        }
+        return new ResponseEntity<User> (user, HttpStatus.OK);
     }
 }
--- src/main/java/com/example/store/entity/User.java
+++ src/main/java/com/example/store/entity/User.java
@@ -7,7 +7,7 @@ import jakarta.persistence.*;
 public class User {
     @Id
     @GeneratedValue(strategy = GenerationType.IDENTITY)
-    @Column(name="userId")
+    @Column(name="user_id")
     private int userId;
 
     @Column(name="name")
@@ -16,15 +16,22 @@ public class User {
     @Column(name="age")
     private int age;
 
+    @Column(name="email")
+    private String email;
+
+    @Column(name="password")
+    private String password;
+
     public User() {
         super();
     }
 
-    public User(int userId, String name, int age) {
-        super();
+    public User(int userId, String name, int age, String email, String password) {
         this.userId = userId;
         this.name = name;
         this.age = age;
+        this.email = email;
+        this.password = password;
     }
 
     public int getUserId() {
@@ -39,6 +46,14 @@ public class User {
         return age;
     }
 
+    public String getEmail() {
+        return email;
+    }
+
+    public String getPassword() {
+        return password;
+    }
+
     public void setUserId(int userId) {
         this.userId = userId;
     }
@@ -51,12 +66,22 @@ public class User {
         this.age = age;
     }
 
+    public void setEmail(String email) {
+        this.email = email;
+    }
+
+    public void setPassword(String password) {
+        this.password = password;
+    }
+
     @Override
     public String toString() {
         return "User{" +
                 "userId=" + userId +
                 ", name='" + name + '\'' +
                 ", age=" + age +
+                ", email='" + email + '\'' +
+                ", password='" + password + '\'' +
                 '}';
     }
 }
